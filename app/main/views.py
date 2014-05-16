dfa42619779c86462609fe2af0953d1f35517020
Georg Walther <georgwalther@gmail.com>
2014-05-16 16:04:42 +0100

    add PubMed query and return PMID's

--- app/main/views.py
+++ app/main/views.py
@@ -4,14 +4,36 @@ from . import main
 from .forms import KeywordsForm
 from .. import db
 from ..models import Article
+import requests
+from lxml import etree
+
+ENTREZ_ADDRESS = ('http://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
+                  'esearch.fcgi?db=pubmed&term=PLoS%20*[journal]')
+ENTREZ_OPTIONS = ['&usehistory=y']
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
     form = KeywordsForm()
     if form.validate_on_submit():
-        session['keywords'] = form.keywords.data.split(' ')
+        session['keywords'] = form.keywords.data.split()
+        session['query'] = ENTREZ_ADDRESS + '+AND+'.join(session['keywords'])
+        session['query'] += ''.join(ENTREZ_OPTIONS)
+
+        # query PubMed for PMID's that match keywords
+        r = requests.get(session.get('query', ''))
+        if r.status_code == 200:
+            xml = etree.fromstring(r.text)
+            try:
+                pmids = xml.xpath('//IdList//Id')
+                pmids = [pmid.text for pmid in pmids]
+                session['pmids'] = pmids
+            except:
+                pass
+
         return redirect(url_for('.index'))
+
     return render_template('index.html',
                            form=form,
                            current_time=datetime.utcnow(),
-                           keywords=session.get('keywords'))
+                           keywords=session.get('keywords'),
+                           pmids=session.get('pmids'))
